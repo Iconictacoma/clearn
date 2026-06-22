dc8a60903bea7d1db4a954dc30c7845329fb2394
Iconictacoma <sevonprince371@gmail.com>
2026-06-22 13:04:58 -0400

    Create DATATYPEEXAMPLE.c

---
+++ DATA/DATATYPEEXAMPLE.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int students = 100;
+
+    if (students > 100) {
+        printf("Large school\n");
+    } else {
+        printf("Small school\n");
+    }
+}
